a34636535529aa898313fa2c256228b3bd8109ae
Kadir <kadir.albostan@gmail.com>
2020-12-07 12:14:15 +0100

    symfony08

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,24 +15,39 @@ class ProgramController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @return Response A response inrance
      */
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+       $programs = $this->getDoctrine()
+           ->getRepository(Program::class)
+           ->findAll();
+
+       return $this->render(
+           'program/index.html.twig',
+           ['programs' => $programs]
+       );
     }
 
     /**
+     * Getting a program by id
      *
-     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
-     * @param int $id
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
      * @return Response
      */
     public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program){
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }

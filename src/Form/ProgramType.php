1bb20eeade03c39bc626a692969d1c1db24f28ee
Kadir <kadir.albostan@gmail.com>
2020-12-15 16:52:48 +0100

    symfony15

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -20,8 +22,14 @@ class ProgramType extends AbstractType
             ->add('summary', TextareaType::class)
             ->add('poster', UrlType::class)
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+                ]);
     }
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
